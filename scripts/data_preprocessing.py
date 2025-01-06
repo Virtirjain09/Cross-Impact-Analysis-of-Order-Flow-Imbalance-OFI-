9d1d851d56a506ee37b15bff85ac3f6fc5becfaf
Virtirjain09 <virti.jain@stonybrook.edu>
2025-01-05 20:27:56 -0500

    data_preprocessing.py

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,1 +1,17 @@
-hello
+import pandas as pd
+import numpy as np
+
+def preprocess_data(data):
+    data.dropna(inplace=True)
+    data['timestamp'] = pd.to_datetime(data['ts_recv'])
+    data.sort_values(by=['symbol', 'timestamp'], inplace=True)
+    data.drop_duplicates(inplace=True)
+    return data
+
+def clean_ofi_data(ofi_data):
+    important_columns = ['symbol', 'ofi_pca', 'price_change', 'future_price_change_1min', 'future_price_change_5min', 'ofi_lagged']
+    numeric_columns = [col for col in important_columns if col != 'symbol']
+    ofi_data_cleaned = ofi_data[important_columns].copy()
+    ofi_data_cleaned[numeric_columns] = ofi_data_cleaned[numeric_columns].replace([np.inf, -np.inf], np.nan)
+    ofi_data_cleaned = ofi_data_cleaned.dropna()
+    return ofi_data_cleaned
